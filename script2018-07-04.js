90a2cf6e2ec6b788e5517a9b6d545093d6faee6d
Kevin Liao <liao1k@gmail.com>
2018-07-05 08:26:19 -0400

    Using asynchronous FileReader() to read file and launch fetch from loaded event handler after FileReader finishes its reading operation

--- script2018-07-04.js
+++ script2018-07-04.js
@@ -2,23 +2,14 @@
 var uri = 'https://ipshare.info/ipfs/QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn/';
 var fileContents = 'This is my Story';
 
-
-
-
-
-
-
 //const buttonElement = document.getElementById('saveButtonId');
 //buttonElement.addEventListener('click', function (event) {
 //   alert('Element clicked through function!');
 // });
 function Save() {
     //var fs = requestFileSystemSync(d)
-
-    //var fileContents = selectedFile.getAsBinary();
-
     var selectedFile = document.getElementById('uploadFileId').files[0];
-    var uploadFileName = selectedFile.name;
+    //var fileContents = selectedFile.getAsBinary();
 
     var reader = new FileReader();
     reader.readAsArrayBuffer(selectedFile);
@@ -31,47 +22,22 @@ function Save() {
     reader.onload = loaded;
     reader.onerror = errorHandler;
 
-
+    var uploadFileName = selectedFile.name;
     var fileUploadForm = document.getElementById('uploadFileFormId');
     var formData = new FormData(fileUploadForm);
     //formData.append('uploadFiles', selectedFile, uploadFileName);
-
-
+    uri = uri + uploadFileName;
+    let h = new Headers();
+    h.append('Accept', '*/*');
     //let inputTextValue = document.getElementById('inputTextId').value;
     console.log('selectedFile:', selectedFile);
-    
+    console.log('URI:', uri);
     console.log('uploadFileName:', uploadFileName);
     console.log('FORMDATA:', formData);
     console.log('This is fileContents:', fileContents);
     console.log('Form Entries are:', formData.entries());
-}
 
 
-function updateProgress(evt) {
-    if (evt.lengthComputable) {
-        // evt.loaded and evt.total are ProgressEvent properties
-        var loaded = (evt.loaded / evt.total);
-        if (loaded < 1) {
-            // Increase the prog bar length
-            // style.width = (loaded * 200) + "px";
-        }
-    }
-}
-
-function loaded(evt) {
-    // Obtain the read file data
-
-    var selectedFile = document.getElementById('uploadFileId').files[0];
-    var uploadFileName = selectedFile.name;
-
-    uri = uri + uploadFileName;
-    let h = new Headers();
-    h.append('Accept', '*/*');
-
-
-    fileContents = evt.target.result;
-    console.log('URI inside loaded event handler:', uri);
-    console.log('FileContents inside loaded event handler:', fileContents);
     let req = new Request(uri, {
         method: 'PUT',
         headers: h,
@@ -96,6 +62,26 @@ function loaded(evt) {
         .catch((error) => {
             console.log('ERROR:', error.message);
         });
+
+
+}
+
+
+function updateProgress(evt) {
+    if (evt.lengthComputable) {
+        // evt.loaded and evt.total are ProgressEvent properties
+        var loaded = (evt.loaded / evt.total);
+        if (loaded < 1) {
+            // Increase the prog bar length
+            // style.width = (loaded * 200) + "px";
+        }
+    }
+}
+
+function loaded(evt) {
+    // Obtain the read file data
+    fileContents = evt.target.result;
+    console.log('FileContents inside loaded event handler:', fileContents);
 }
 
 function errorHandler(evt) {
@@ -105,6 +91,6 @@ function errorHandler(evt) {
 }
 
 
-let SaveButton = document.getElementById("saveButtonId");
-SaveButton.onclick = Save;
+//let SaveButton = document.getElementById("saveButtonId");
+//SaveButton.onclick = Save;
 
