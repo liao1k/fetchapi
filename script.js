89ebf66ebad767ff9ca66cadc9943555d259ff8c
Kevin Liao <liao1k@gmail.com>
2018-07-07 06:13:13 -0400

    clean up script.js code get rid of junk statements

--- script.js
+++ script.js
@@ -1,45 +1,24 @@
 "use strict";
 
 var uri = 'https://ipshare.info/ipfs/QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn/';
-//var fileContents = 'This is my Story';
 
-//const buttonElement = document.getElementById('saveButtonId');
-//buttonElement.addEventListener('click', function (event) {
-//   alert('Element clicked through function!');
-// });
 function Save() {
-    //var fs = requestFileSystemSync(d)
-
-    //var fileContents = selectedFile.getAsBinary();
-
     var selectedFiles = document.getElementById('uploadFileId');
     var selectedAFile = selectedFiles.files[0];
     var uploadFileName = selectedAFile.name;
 
     var reader = new FileReader();
     reader.readAsArrayBuffer(selectedAFile);
-    //reader.readAsBinaryString(selectedFile);
 
-    //console.log('fileContents by Globally declared variable:', fileContents);
     reader.onload = loaded;
-    //fileString = loaded;
 
     reader.onprogress = updateProgress;
     reader.onload = loaded;
     reader.onerror = errorHandler;
 
-
-
-    //formData.append('uploadFiles', selectedFile, uploadFileName);
-
-
-    //let inputTextValue = document.getElementById('inputTextId').value;
     console.log('selectedFile:', selectedAFile);
 
     console.log('uploadFileName:', uploadFileName);
-    //console.log('FORMDATA:', formData);
-    //console.log('This is fileContents:', fileContents);
-    //console.log('Form Entries are:', formData.entries());
 }
 
 
@@ -68,9 +47,6 @@ function loaded(evt) {
 
     uri = uri + uploadFileName;
     let h = new Headers();
-    //h.append('Accept', '*/*');
-    //h.append('Content-Type', 'text/plain');
-    //var formDataEntryValues = formData.entries();
 
     //don't use fileContents string to store file contents. 
     //javascript string has up to 265MB limit.
@@ -79,18 +55,16 @@ function loaded(evt) {
     //var fileContents = evt.target.result;
 
     console.log('URI inside loaded event handler:', uri);
-    //console.log('FileContents inside loaded event handler:', fileContents);
     console.log('FormData object inside loaded event handler:', formData);
     //Display the key/value pairs
-    
+
     /*
     for (var pair of formDataEntryValues) {
         console.log('Following are pair from formDataEntry');
         console.log(pair[0] + ', ' + pair[1]);
     }
     */
-   
-    //console.log('entries method of formData object inside loaded event handler:', formDataEntryValues);
+
 
     // save changes to ipfs
 
