88abefb328ea3dad195f21b9f8d46f96be37647e
Kevin Liao <liao1k@gmail.com>
2018-07-08 19:13:49 -0400

    update css style in index.html

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ function loaded(evt) {
             //console.log('IPShare got:', responseText);
             var respHeaders = this.getAllResponseHeaders();
             console.log('resp headers:', respHeaders);
-            var respIpfsHashHeader = this.getResponseHeader('Location');
+            var respIpfsHashHeader = this.getResponseHeader('location');
             console.log('IPFS HEADERS:', respIpfsHashHeader);
             var httpurl = 'https://ipshare.info' + respIpfsHashHeader;
             var htmlAtag = document.getElementById("ipfshashId");
